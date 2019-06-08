8f7ba2be190dcf60dc3f3f7799ea1da959e572d6
ansonfoong <afoong@albany.edu>
2019-06-08 19:30:04 -0400

    Check duplicate edges in addEdge() function

--- src/Graph.java
+++ src/Graph.java
@@ -18,14 +18,26 @@ public class Graph {
 		// Need to check if both Vertices are in the Graph/Map.
 		if(this.map.containsKey(A.nodeData) && this.map.containsKey(B.nodeData)) // If both Nodes are found
 		{
-			this.map.get(A.nodeData).add(new Edge(A, B));
-			this.map.get(B.nodeData).add(new Edge(B, A));
+			if(this.exists(A, B))
+			{
+				System.out.println("Cannot add edge because it already exists.");
+			}
+			else {
+				this.map.get(A.nodeData).add(new Edge(A, B));
+				this.map.get(B.nodeData).add(new Edge(B, A));
+			}
 		}
 		else if(!this.map.containsKey(A.nodeData) && this.map.containsKey(B.nodeData)) // If Node A is not in the Graph, but Node B is.
 		{
-			this.map.put(A.nodeData, new ArrayList<Edge>());
+			this.map.put(A.nodeData, new ArrayList<Edge>());  // Insert new Vertex.
 			this.map.get(A.nodeData).add(new Edge(A, B));
 			this.map.get(B.nodeData).add(new Edge(B, A));
+		}
+		else if(!this.map.containsKey(B.nodeData) && this.map.containsKey(A.nodeData))
+		{
+			this.map.put(B.nodeData, new ArrayList<Edge>());
+			this.map.get(B.nodeData).add(new Edge(B, A));
+			this.map.get(A.nodeData).add(new Edge(A, B));
 		}
 		else {
 			throw new Exception("VertexNode not found.");
@@ -41,6 +53,33 @@ public class Graph {
 		else {
 			throw new Exception("VertexNode not found.");
 		}
+	}
+	public boolean exists(VertexNode A, VertexNode B)
+	{
+		ArrayList<Edge> edges = this.getEdges(A.nodeData);
+		if(edges.isEmpty())
+			return false;
+		else {
+			for(Edge e : edges)
+			{
+				if(e.firstNode.nodeData.equals(A.nodeData) && e.secondNode.nodeData.equals(B.nodeData))
+				{
+					System.out.println("Edge Exists.");
+					return true;
+				}
+				else if(e.firstNode.nodeData.equals(B.nodeData) && e.secondNode.nodeData.equals(A.nodeData)) {
+					System.out.println("Edge Exists.");
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+	public void printEdges(String vertex)
+	{
+		ArrayList<Edge> edges = this.getEdges(vertex);
+		for(Edge edge : edges)
+			System.out.println(edge.firstNode.nodeData + "<->" + edge.secondNode.nodeData);
 	}
 	public void printVertices()
 	{
--- src/GraphDriver.java
+++ src/GraphDriver.java
@@ -5,6 +5,13 @@ public class GraphDriver {
 	{
 		Graph graph = new Graph();
 		// Add all vertices.
-		
+		VertexNode A = new VertexNode("Anson");
+		VertexNode B = new VertexNode("Jack");
+		graph.addVertex("Anson");
+		graph.addVertex("Jack");
+		graph.addEdge(A,  B);
+		graph.addEdge(A, B);
+		graph.addEdge(B, A);
+		graph.printEdges("Jack");
 	}
 }
