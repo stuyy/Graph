f4f28a3786e33de554ccf554bca98d4689eeefbb
ansonfoong <afoong@albany.edu>
2019-06-08 20:46:11 -0400

    addEdge()method will return  if both vertices are equal to each other

--- src/ADTGraph.java
+++ src/ADTGraph.java
@@ -20,14 +20,45 @@ public class ADTGraph {
 			return true;
 		}
 	}
+	/**
+	 * Adds an edge to the Graph with two VertexNodes.
+	 * @param a - First Node
+	 * @param b - Second Node
+	 */
+	public void addEdge(VertexNode a, VertexNode b)
+	{
+		if(a.equals(b)) // For now, we won't allow self-loops of a Vertex.
+		{
+			System.out.println("Cannot have an edge with a Vertex of itself.");
+			return;
+		}
+		if(this.map.containsKey(a)) // If the map contains VertexNode a
+		{
+			ArrayList<VertexNode> edges = this.map.get(a);
+			if(edges.contains(b))
+			{
+				System.out.println("B is already an edge with A");
+				return;
+			}
+			else {
+				System.out.println("No edge. Adding...");
+				this.map.get(a).add(b);
+			}
+		}
+	}
 	
 	public static void main(String [] args)
 	{
 		ADTGraph graph = new ADTGraph();
 		VertexNode a = new VertexNode("A");
-		VertexNode b = new VertexNode("A");
+		VertexNode b = new VertexNode("B");
 		graph.addVertex(a);
 		graph.addVertex(b);
+		graph.addEdge(a, b);
+		
+		ArrayList<VertexNode> edges = graph.map.get(a);
+		for(VertexNode n : edges)
+			System.out.println(n.nodeData);
 		
 	}
 }
